f8cfbbd9a9447dbbe7e680fd71b299ae4a89a7ea
Rolo_ryzen7 <rolo_afrole@outloo.com>
2021-05-06 16:37:03 +0900

    add env info

---
+++ launch.bat
@@ -0,0 +1,12 @@
+@echo off
+if not "%~0"=="%~dp0.\%~nx0" (
+     start /min cmd /c,"%~dp0.\%~nx0" %*
+     exit
+)
+set activatePath=\Anaconda3\Scripts\activate.bat
+set installedPath=%USERPROFILE%
+@REM set installedPath=C:\tools
+cd /d %~dp0
+call %installedPath%%activatePath%
+call activate levelabel
+python main.py
